7c786a7e7eb08f9c0bc872549c94bda134358985
leall <Reinaldolivecoza@outlook.com>
2026-08-14 22:57:43 -0300

    Add files via upload

--- api/proxy.js
+++ api/proxy.js
@@ -8,7 +8,8 @@ export default async function handler(req, res) {
     try {
         const response = await fetch(url, {
             headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+                'User-Agent': 'VLC/3.0.9 LibVLC/3.0.9',
+                'Accept': '*/*'
             }
         });
 
